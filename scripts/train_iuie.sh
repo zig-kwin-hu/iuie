efae513e97e568095d2a7ecef860856a1e478b96
zig-kwin-hu <zikunhu@foxmail.com>
2024-02-10 16:37:39 +0800

    script

--- scripts/train_iuie.sh
+++ scripts/train_iuie.sh
@@ -5,7 +5,7 @@ epoch_map=([with_sentence_iuie_mean_of_encoder]=30 [NYT11_NYT]=10 [semval-RE]=10
 # declare -A TASK2DATASETS=([re]="conll04 SciERC NYT11 semval-RE ADE_corpus-1500" [eet]="ace phee casie" [eea]="ace phee casie" [ner]="CoNLL_2003 ACE_2004 ACE_2005")
 # DONE : [ner] = ACE_2004 ACE_2005 AnatEM bc2gm bc4chemd bc5cdr Broad_Tweet_Corpus CoNLL_2003 FabNER FindVehicle GENIA_NER HarveyNER mit-movie mit-restaurant MultiNERD ncbi Ontonotes_sample_30000 PolyglotNER TweetNER7_sample_15000 WikiANN_en WikiNeural
 #declare -A TASK2DATASETS=([re]="ADE_corpus NYT11_sample_30000 New-York-Times-RE_sample_30000 semval-RE conll04 GIDS SciERC kbp37" [eet]="ace phee casie" [eea]="ace phee casie" [ner]="ACE_2004 ACE_2005 AnatEM bc2gm bc4chemd bc5cdr Broad_Tweet_Corpus CoNLL_2003 FabNER FindVehicle GENIA_NER HarveyNER mit-movie mit-restaurant MultiNERD ncbi Ontonotes_sample_30000 PolyglotNER TweetNER7 WikiANN_en WikiNeural")
-declare -A TASK2DATASETS=([ner]="Broad_Tweet_Corpus WikiNeural_sample_20000" [re]="SciERC" [with_sentence_iuie_mean_of_encoder]="0_2" [ner_cluster]="ACE_2004_ACE_2005" [re_cluster]="NYT11_NYT" [eet]="ace phee casie" [eea]="ace phee casie")
+declare -A TASK2DATASETS=([ner]="plo_all" [re]="SciERC" [with_sentence_iuie_mean_of_encoder]="0_2" [ner_cluster]="ACE_2004_ACE_2005" [re_cluster]="NYT11_NYT" [eet]="ace phee casie" [eea]="ace phee casie")
 
 set -x
 
@@ -15,7 +15,7 @@ export TRANSFORMERS_CACHE=./huggingface
 port=$(shuf -i25000-30000 -n1)
 
 model_name_or_path=ZWK/InstructUIE
-lora_r=16
+lora_r=64
 lora_alpha=16
 add_name=True
 # model_name_or_path=google/flan-t5-xxl
@@ -52,11 +52,11 @@ do
         --min_positive_labels -1 \
         --output_dir "${output_dir}" \
         --input_record_file iuie.record \
-        --per_device_train_batch_size 10 \
-        --per_device_eval_batch_size 16 \
-        --gradient_accumulation_steps 3 \
+        --per_device_train_batch_size 5 \
+        --per_device_eval_batch_size 8 \
+        --gradient_accumulation_steps 6 \
         --learning_rate 5e-05 \
-        --num_train_epochs 1 \
+        --num_train_epochs 4 \
         --run_name ${model_name_or_path}-${TASK_CONFIG}-${DATASET_CONFIG} \
         --max_source_length 256 \
         --max_target_length 50 \
